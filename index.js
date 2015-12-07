1f64cc08e691eaddc3100e036adbbf4046f6ad2b
Adrian Falleiro <adrian@falleiro.com>
2015-12-07 19:48:43 +1100

    Rewrite using through2; Throw error on unsuccessful upload (for CI/CD)

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
 'use strict';
 
-var es = require('event-stream');
+// Deps
 var knox = require('knox');
 var gutil = require('gulp-util');
+var through = require('through2');
 var mime = require('mime');
+
+// Defaults
 mime.default_type = 'text/plain';
 
 module.exports = function (aws, options) {
-  options = options || {};
+    
+    options = options || {};
 
-  if (!options.delay) { options.delay = 0; }
+    if (!options.delay) { options.delay = 0; }
 
-  var client = knox.createClient(aws);
-  var waitTime = 0;
-  var regexGzip = /\.([a-z]{2,})\.gz$/i;
-  var regexGeneral = /\.([a-z]{2,})$/i;
+    var client = knox.createClient(aws);
+    var waitTime = 0;
+    var regexGzip = /\.([a-z]{2,})\.gz$/i;
+    var regexGeneral = /\.([a-z]{2,})$/i;
 
-  return es.mapSync(function (file) {
 
-      // Verify this is a file
-      if (!file.isBuffer()) { return file; }
+    return through.obj(function (file, enc, cb) {
 
-      var uploadPath = file.path.replace(file.base, options.uploadPath || '');
-      uploadPath = uploadPath.replace(new RegExp('\\\\', 'g'), '/');
-      var headers = { 'x-amz-acl': 'public-read' };
-      if (options.headers) {
-          for (var key in options.headers) {
-              headers[key] = options.headers[key];
-          }
-      }
-
-      if (regexGzip.test(file.path)) {
-          // Set proper encoding for gzipped files, remove .gz suffix
-          headers['Content-Encoding'] = 'gzip';
-          uploadPath = uploadPath.substring(0, uploadPath.length - 3);
-      } else if (options.gzippedOnly) {
-          // Ignore non-gzipped files
-          return file;
-      }
-
-      // Set content type based of file extension
-      if (!headers['Content-Type'] && regexGeneral.test(uploadPath)) {
-        headers['Content-Type'] = mime.lookup(uploadPath);
-        if (options.encoding) {
-          headers['Content-Type'] += '; charset=' + options.encoding;
+        if (file.isNull()) {
+            return cb(null, file)
         }
-      }
 
-      headers['Content-Length'] = file.stat.size;
-
-      client.putBuffer(file.contents, uploadPath, headers, function(err, res) {
-        if (err || res.statusCode !== 200) {
-          gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
-        } else {
-          gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
-          res.resume();
+        if (file.isStream()) {
+            return cb(new gutil.PluginError('gulp-s3-publish', 'Streaming not supported'))
         }
-      });
 
-      return file;
-  });
-};
+        try {
+            var uploadPath = file.path.replace(file.base, options.uploadPath || '')
+               .replace(new RegExp('\\\\', 'g'), '/') 
+
+
+            // Explicitly set headers
+            // Else default to public access for all files
+            var headers = (options.headers)
+                ? options.headers
+                : { 'x-amz-acl' : 'public-read' }
+
+            if (regexGzip.test(file.path)) {
+                // Set proper encoding for gzipped files, remove .gz suffix
+                headers['Content-Encoding'] = 'gzip';
+                uploadPath = uploadPath.substring(0, uploadPath.length - 3);
+            } else if (options.gzippedOnly) {
+                // Ignore non-gzipped files
+                return cb(null, file)
+            }
+
+            // Set content type based of file extension
+            if (!headers['Content-Type'] && regexGeneral.test(uploadPath)) {
+                headers['Content-Type'] = mime.lookup(uploadPath);
+                if (options.encoding) {
+                    headers['Content-Type'] += '; charset=' + options.encoding;
+                }
+            }
+
+            headers['Content-Length'] = file.stat.size;
+
+            // Prepare upload callback
+            var uploadCallback = function(err, res){
+                if (err || res.statusCode !== 200) {
+                    cb(new gutil.PluginError('gulp-s3-publish', "[" + res.statusCode + "] " + file.path + " -> " + uploadPath))
+
+                } else {
+                    gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
+                }
+
+                cb(null, file)  
+            }
+
+            // Lets Upload
+            client.putBuffer(file.contents, uploadPath, headers, uploadCallback.bind(this))
+
+        } catch (err) {
+            this.emit('error', new gutil.PluginError('gulp-s3-publish', err.toString()));
+        }
+    })
+}
