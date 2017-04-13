1b0f7c7f6e514737f03c4fc094ecac3f42ec4c72
Adrian Falleiro <adrian.falleiro@analogfolk.com>
2017-04-13 12:01:42 +1000

    Refactors to use `aws-sdk` and support V4 signing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 // Deps
-var knox = require('knox');
+
 var gutil = require('gulp-util');
 var through2Concurrent = require('through2-concurrent');
 var mime = require('mime');
 var assign = require('lodash.assign');
+var AWS = require('aws-sdk');
 
 // Defaults
 mime.default_type = 'text/plain';
@@ -19,18 +20,21 @@ module.exports = function (aws, opts) {
 
     var options = assign({}, defaults, opts);
 
-
-
     if (!options.delay) { options.delay = 0; }
     if (!options.parallel) { options.parallel = 1; }
 
-    var client = knox.createClient(aws);
+    var client = new AWS.S3({
+        apiVersion: '2006-03-01',
+        accessKeyId: aws.key,
+        secretAccessKey: aws.secret,
+        region: aws.region,
+    });
+
     var waitTime = 0;
     var regexGzip = /\.([a-z0-9]{2,})\.gz$/i;
     var regexGeneral = /\.([a-z0-9]{2,})$/i;
 
-
-    return through2Concurrent.obj({maxConcurrency: options.concurrency}, function (file, enc, cb) {
+    return through2Concurrent.obj({ maxConcurrency: options.concurrency }, function (file, enc, cb) {
 
         if (file.isNull()) {
             return cb(null, file)
@@ -44,49 +48,52 @@ module.exports = function (aws, opts) {
             var uploadPath = file.path.replace(file.base, options.uploadPath || '')
                .replace(new RegExp('\\\\', 'g'), '/') 
 
-
             // Explicitly set headers
             // Else default to public access for all files
             var headers = (options.headers)
                 ? options.headers
-                : { 'x-amz-acl' : 'public-read' }
+                : { ACL : 'public-read' }
 
             if (regexGzip.test(file.path)) {
                 // Set proper encoding for gzipped files, remove .gz suffix
-                headers['Content-Encoding'] = 'gzip';
+                headers['ContentEncoding'] = 'gzip';
                 uploadPath = uploadPath.substring(0, uploadPath.length - 3);
             } else if (options.gzippedOnly) {
                 // Ignore non-gzipped files
-                return cb(null, file)
+                return cb(null, file);
             }
 
             // Set content type based of file extension
-            if (!headers['Content-Type'] && regexGeneral.test(uploadPath)) {
-                headers['Content-Type'] = mime.lookup(uploadPath);
+            if (!headers['ContentType'] && regexGeneral.test(uploadPath)) {
+                headers['ContentType'] = mime.lookup(uploadPath);
                 if (options.encoding) {
-                    headers['Content-Type'] += '; charset=' + options.encoding;
+                    headers['ContentType'] += '; charset=' + options.encoding;
                 }
             }
 
-            headers['Content-Length'] = file.stat.size;
+            headers['ContentLength'] = file.stat.size;
 
             // Prepare upload callback
             var uploadCallback = function(err, res){
-                if (err || res.statusCode !== 200) {
-                    cb(new gutil.PluginError('gulp-s3-publish', "[" + res.statusCode + "] " + file.path + " -> " + uploadPath))
+                if (err) {
+                    cb(new gutil.PluginError('gulp-s3-publish', "[" + res.statusCode + "] " + file.path + " -> " + uploadPath));
 
                 } else {
                     gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
                 }
 
-                cb(null, file)  
+                cb(null, file);
             }
 
             // Lets Upload
-            client.putBuffer(file.contents, uploadPath, headers, uploadCallback.bind(this))
+
+            headers['Bucket'] = aws.bucket;
+            headers['Key'] = uploadPath;
+            headers['Body'] = file.contents;
+            client.putObject(headers, uploadCallback);
 
         } catch (err) {
             this.emit('error', new gutil.PluginError('gulp-s3-publish', err.toString()));
         }
-    })
+    });
 }
